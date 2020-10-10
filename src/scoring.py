2a7e22820dfabb6f6b3a5d5f61bfc12f13208e95
Jerrad Genson <contact@jerradgenson.com>
2020-10-10 23:37:19 +0200

    Split util.py off from gen_model.py

--- src/scoring.py
+++ src/scoring.py
@@ -1,3 +1,14 @@
+"""
+A library of functions for scoring models.
+
+Copyright 2020 Jerrad M. Genson
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+"""
+
 import logging
 
 import numpy as np
