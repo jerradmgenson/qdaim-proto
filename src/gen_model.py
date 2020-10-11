25306380b526eb464e958ec48512c8e86baa99b8
Jerrad Genson <contact@jerradgenson.com>
2020-10-11 22:18:32 +0200

    Add docstrings to find_outliers and score_outliers

--- src/gen_model.py
+++ src/gen_model.py
@@ -172,11 +172,12 @@ def main(argv):
 
     outlier_scores = None
     if command_line_arguments.outlier_scores:
-        outlier_scores = score_outliers(model, datasets)
+        outlier_scores, outlier_count, outlier_model = score_outliers(model, datasets)
+        model.outlier_model = outlier_model
 
     bind_model_metadata(model, model_scores,
                         cross_validation_scores=cross_validation_scores,
-                        outlier_scores=outlier_scores)
+                        outlier_scores=(outlier_scores, outlier_count))
 
     predictions = model.predict(datasets.validation.inputs)
     validation_dataset = create_validation_dataset(datasets.validation.inputs,
@@ -252,7 +253,7 @@ def bind_model_metadata(model, scores,
       scores: A scores dict returned by `score_model`.
       cross_validation_scores: (Default=None) A 3-tuple of cross-validation scores
                                consisting of (median_scores, mad_scores, n_splits).
-      outlier_scores: (Default=None) A scores dict returned by `score_model`.
+      outlier_scores: (Default=None) A 2-tuple of (scores dict, outlier count).
 
     Returns
       None
@@ -274,8 +275,9 @@ def bind_model_metadata(model, scores,
 
     if outlier_scores:
         print('\nOutlier scores:')
+        print(f'Outliers:  {outlier_scores[1]}')
         score_count = 0
-        for metric, score in outlier_scores.items():
+        for metric, score in outlier_scores[0].items():
             if score:
                 score_count += 1
                 setattr(model, 'outlier_' + metric, score)
@@ -415,7 +417,7 @@ def cross_validate(model, datasets, n_splits):
     sets numerous times and calculating summary statistics for the model scores.
 
     Args:
-      model: An trained instance of a scikit-learn estimator.
+      model: A trained instance of a scikit-learn estimator.
       datasets: An instance of Datasets.
       n_splits: Number of splits (or folds) to use in cross-validation.
 
@@ -466,22 +468,61 @@ def cross_validate(model, datasets, n_splits):
 
 
 def score_outliers(model, datasets):
+    """
+    Score model on only the outliers in a dataset.
+
+    Args:
+      model: A trained instance of a scikit-learn estimator.
+      datasets: An instance of Datasets.
+
+    Returns:
+      A scores dict returned by `score_model`.
+
+    """
+
     if hasattr(model, 'steps') and len(model.steps) >= 2:
         # Assume that `model` is a pipeline and the first step is preprocessing.
-        outliers = find_anomalies(datasets.training.inputs,
-                                  datasets.validation.inputs,
-                                  preprocessor=model.steps[0][1])
+        outliers, outlier_model = find_outliers(datasets.training.inputs,
+                                                datasets.validation.inputs,
+                                                preprocessor=model.steps[0][1])
 
     else:
-        outliers = find_anomalies(datasets.training.inputs,
-                                  datasets.validation.inputs)
+        outliers, outlier_model = find_outliers(datasets.training.inputs,
+                                                datasets.validation.inputs)
 
-    return scoring.score_model(model,
-                               datasets.validation.inputs[outliers],
-                               datasets.validation.targets[outliers])
+    outlier_count = np.sum(outliers)
+    scores = scoring.score_model(model,
+                                 datasets.validation.inputs[outliers],
+                                 datasets.validation.targets[outliers])
+
+    return scores, outlier_count, outlier_model
 
 
-def find_anomalies(x_train, x_test, preprocessor=None):
+def find_outliers(x, y, preprocessor=None, p=.001):
+    """
+    Find outliers in array `y` based on core samples in array `x`.
+
+    Finds outliers using an anomaly detection system approach. First, it uses
+    density-based spacial clustering of applications with noise (DBSCAN) to find
+    core samples in the training dataset. Then it calculates the Euclidean
+    distance from each sample in the testing dataset to the nearest core sample
+    in the training dataset. Finally, it performs a Chi-Squared test to determine
+    if the distances between the core samples and any of the testing samples is
+    statistically significant. It considers any samples it finds to be outliers.
+
+    Args:
+      x: An n x m array of training input samples.
+      y: A k x m array of testing input samples.
+      preprocessor: (Default=None) A scikit-learn Transformer object to use in a
+                    pipeline with the DBSCAN model.
+      p: (Default=.001) The significance level to use in the Chi-Squared test
+         for outliers.
+
+    Returns:
+      A k x 1 boolean array where True values correspond to outliers in `y`.
+
+    """
+
     pipeline_steps = []
     if preprocessor:
         pipeline_steps.append(('preprocessor', preprocessor))
@@ -490,23 +531,24 @@ def find_anomalies(x_train, x_test, preprocessor=None):
     pipeline = Pipeline(steps=pipeline_steps)
     parameter_grid = dict(
         model__eps=[0.05, 0.1, 0.5, 1, 5],
-        model__min_samples=list(range(2, int(math.sqrt(len(x_train)))))
+        model__min_samples=list(range(2, int(math.sqrt(len(x)))))
     )
 
     grid_estimator = sklearn.model_selection.GridSearchCV(pipeline,
                                                           parameter_grid,
                                                           scoring=scoring.mean_silhouette_coefficient)
 
-    grid_estimator.fit(x_train)
-    core_samples = grid_estimator.best_estimator_.steps[-1][1].components_
+    grid_estimator.fit(x)
+    model = grid_estimator.best_estimator_.steps[-1][1]
+    core_samples = model.components_
     if preprocessor:
-        x_test = preprocessor.transform(x_test)
+        y = preprocessor.transform(y)
 
-    distances = distance.cdist(x_test, core_samples)
+    distances = distance.cdist(y, core_samples)
     min_distances = distances.min(axis=1)
-    p_values = 1 - chi2.cdf(min_distances, len(x_test[0]) - 1)
+    p_values = 1 - chi2.cdf(min_distances, len(y[0]) - 1)
 
-    return p_values < .001
+    return p_values < p, model
 
 
 if __name__ == '__main__':  # pragma: no cover
