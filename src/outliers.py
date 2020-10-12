f98a2cbcb26a7f3c92527959c3f59683714ff83b
Jerrad Genson <contact@jerradgenson.com>
2020-10-12 13:54:18 +0200

    Add function for finding outliers based on Mahalanobis distance

--- src/outliers.py
+++ src/outliers.py
@@ -23,6 +23,7 @@ def score(model, datasets):
 
     """
 
+    """
     if hasattr(model, 'steps') and len(model.steps) >= 2:
         # Assume that `model` is a pipeline and the first step is preprocessing.
         outliers, outlier_model = spacial_clustering(datasets.training.inputs,
@@ -32,13 +33,18 @@ def score(model, datasets):
     else:
         outliers, outlier_model = spacial_clustering(datasets.training.inputs,
                                                      datasets.validation.inputs)
+    """
+
+    outliers = mahalanobis_distance(datasets.training.inputs,
+                                    datasets.validation.inputs)
 
     outlier_count = np.sum(outliers)
     scores = scoring.score_model(model,
                                  datasets.validation.inputs[outliers],
                                  datasets.validation.targets[outliers])
 
-    return scores, outlier_count, outlier_model
+    #return scores, outlier_count, outlier_model
+    return scores, outlier_count, outliers
 
 
 def spacial_clustering(a1, a2=None, preprocessor=None):
@@ -52,6 +58,13 @@ def spacial_clustering(a1, a2=None, preprocessor=None):
     greater than the eps parameter chosen for the DBSCAN model. It considers any
     such samples to be outliers.
 
+    This is a robust method of locating outliers - it does not require that the
+    data conform to any particular distribution, it does not require that the
+    inverse covariance matrix be defined, and it is insensitive to noise.
+    However, as a heuristical method, it is less statistically rigorous than
+    other methods and is also comparatively expensive in terms of space and time
+    complexity.
+
     Args:
       a1: n x m array to use as the basis for the core samples.
       a2: k x m array of samples to test for outliers. (Default=a1)
@@ -59,7 +72,9 @@ def spacial_clustering(a1, a2=None, preprocessor=None):
                     constructing the DBSCAN model. (Default=None)
 
     Returns:
-      A k x 1 boolean array where True elements correspond to outliers in a2.
+      A 2-tuple of
+      (k x 1 boolean array where True elements correspond to outliers in a2,
+       DBSCAN model that was used to produce core samples)
 
     """
 
@@ -91,3 +106,39 @@ def spacial_clustering(a1, a2=None, preprocessor=None):
     min_distances = distances.min(axis=1)
 
     return min_distances > model.eps, grid_estimator.best_estimator_
+
+
+def mahalanobis_distance(a1, a2=None, p=.003):
+    """
+    Find outliers in a multivariate system using the Mahalanobis distance.
+
+    Calculates the Mahalanobis distance from each sample in a2 to a1, then
+    performs a Chi-Squared test on each distance to determine the p-value.
+    It considers any samples with a p-value less than the given significance
+    level to be outliers.
+
+    This is a fast and statistically rigorous method of finding outliers in a
+    multivariate system where the features may be correlated. However, it
+    requires that the data conform to an elliptical distribution and that the
+    inverse covariance matrix is defined for a1. Otherwise, it will fail to find
+    the outliers.
+
+    Args:
+      a1: n x m array to use as the distribution for calculating the
+          Mahalanobis distance.
+      a2: k x m array of samples to test for outliers. (Default=a1)
+      p: Significance level to use for determining if a sample is an outlier.
+         (Default=.003)
+
+    Returns:
+      k x 1 boolean array where True elements correspond to outliers in a2.
+
+    """
+
+    if a2 is None:
+        a2 = a1
+
+    distances = distance.cdist(a2, a1, metric='mahalanobis').diagonal()
+    p_values = chi2.cdf(distances, len(a2[0]) - 1)
+
+    return p_values < p
