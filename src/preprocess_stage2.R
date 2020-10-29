4f72814fb06fad36ecf12b3d96804c53dd0fc5f8
Jerrad <contact@jerradgenson.com>
2020-10-29 21:57:49 +0100

    Fix issues found by lintr in preprocess_stage2.R

--- src/preprocess_stage2.R
+++ src/preprocess_stage2.R
@@ -7,7 +7,7 @@
 # gen_model.py.
 #
 # Preprocessing steps performed by this script include:
-# - Discard all columns except those in SUBSET_COLUMNS.
+# - Discard all columns except those in subset_columns.
 # - Discard all rows where trestbps is equal to 0.
 # - Convert cp to a binary class.
 # - Convert restecg to a binary class.
@@ -22,24 +22,21 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-options(error=traceback)
+options(error = traceback)
 
 library(argparser)
 
-# Path to the root of the git repository
-GIT_ROOT <- system2("git", args=c("rev-parse", "--show-toplevel"), stdout=TRUE)
-
 # Name of the testing dataset
-TESTING_DATASET_NAME <- "testing.csv"
+testing_dataset_name <- "testing.csv"
 
 # Name of the training dataset
-TRAINING_DATASET_NAME <- "training.csv"
+training_dataset_name <- "training.csv"
 
 # Name of the validation dataset
-VALIDATION_DATASET_NAME <- "validation.csv"
+validation_dataset_name <- "validation.csv"
 
 # Columns to subset from the original input dataset
-SUBSET_COLUMNS <- c("age", "sex", "cp", "thalrest", "trestbps", "restecg",
+subset_columns <- c("age", "sex", "cp", "thalrest", "trestbps", "restecg",
                     "fbs", "thalach", "exang", "oldpeak", "target")
 
 
@@ -53,37 +50,37 @@ parse_command_line <- function(argv) {
     #  The output of parse_args().
     parser <- arg_parser("Stage 2 preprocessor")
     parser <- add_argument(parser, "target",
-                           help="Path of the directory to output the result of Stage 2 preprocessing.")
+                           help = "Path of the directory to output the result of Stage 2 preprocessing.")
 
     parser <- add_argument(parser, "source",
-                           help="CSV dataset file output by preprocess_stage1.py.")
+                           help = "CSV dataset file output by preprocess_stage1.py.")
 
     parser <- add_argument(parser, "--random-seed",
-                           default=1,
-                           help="Set the number number generator seed.")
+                           default = 1,
+                           help = "Set the number number generator seed.")
 
     parser <- add_argument(parser, "--classification-type",
-                           default="binary",
-                           help="Classification type. Possible values: 'binary', 'ternary', 'multiclass'")
+                           default = "binary",
+                           help = "Classification type. Possible values: 'binary', 'ternary', 'multiclass'")
 
     parser <- add_argument(parser, "--testing-fraction",
-                           default=0.2,
-                           help="Fraction of data to use for testing as a real number between 0 and 1.")
+                           default = 0.2,
+                           help = "Fraction of data to use for testing as a real number between 0 and 1.")
 
     parser <- add_argument(parser, "--validation-fraction",
-                           default=0.2,
-                           help="Fraction of data to use for validation as a real number between 0 and 1.")
+                           default = 0.2,
+                           help = "Fraction of data to use for validation as a real number between 0 and 1.")
 
-    parse_args(parser, argv=argv)
+    parse_args(parser, argv = argv)
 }
 
 
-command_line_arguments <- parse_command_line(commandArgs(trailingOnly=TRUE))
+command_line_arguments <- parse_command_line(commandArgs(trailingOnly = TRUE))
 set.seed(command_line_arguments$random_seed)
 dataset <- read.csv(command_line_arguments$source)
-data_subset <- dataset[, SUBSET_COLUMNS]
+data_subset <- dataset[, subset_columns]
 data_subset <- na.omit(data_subset)
-data_subset <- data_subset[data_subset$trestbps != 0,]
+data_subset <- data_subset[data_subset$trestbps != 0, ]
 
 # Convert chest pain to a binary class.
 data_subset$cp[data_subset$cp != 4] <- 1
@@ -110,21 +107,37 @@ if (command_line_arguments$classification_type == "binary") {
 
 } else if (command_line_arguments$classification_type != "multiclass") {
     # Invalid classification type.
-    stop(sprintf("Unknown classification type `%s`.", command_line_arguments$classification_type))
+    stop(sprintf("Unknown classification type `%s`.",
+                 command_line_arguments$classification_type))
 }
 
 # Shuffle order of rows in dataset.
-data_subset <- data_subset[sample(nrow(data_subset)),]
+data_subset <- data_subset[sample(nrow(data_subset)), ]
 
-testing_rows <- ceiling(nrow(data_subset) * command_line_arguments$testing_fraction)
-validation_rows <- ceiling(nrow(data_subset) * command_line_arguments$validation_fraction) + testing_rows
-testing_data <- data_subset[1:testing_rows,]
-validation_data <- data_subset[(testing_rows+1):validation_rows,]
-training_data <- data_subset[(validation_rows+1):nrow(data_subset),]
+testing_rows <- ceiling(nrow(data_subset)
+                        * command_line_arguments$testing_fraction)
 
-testing_path <- file.path(command_line_arguments$target, TESTING_DATASET_NAME)
-write.csv(testing_data, file=testing_path, quote=FALSE, row.names=FALSE)
-validation_path <- file.path(command_line_arguments$target, VALIDATION_DATASET_NAME)
-write.csv(validation_data, file=validation_path, quote=FALSE, row.names=FALSE)
-training_path <- file.path(command_line_arguments$target, TRAINING_DATASET_NAME)
-write.csv(training_data, file=training_path, quote=FALSE, row.names=FALSE)
+validation_rows <-
+    ceiling(nrow(data_subset)
+            * command_line_arguments$validation_fraction) + testing_rows
+
+testing_data <- data_subset[1:testing_rows, ]
+validation_data <- data_subset[(testing_rows + 1):validation_rows, ]
+training_data <- data_subset[(validation_rows + 1):nrow(data_subset), ]
+
+testing_path <- file.path(command_line_arguments$target, testing_dataset_name)
+write.csv(testing_data, file = testing_path, quote = FALSE, row.names = FALSE)
+validation_path <- file.path(command_line_arguments$target,
+                             validation_dataset_name)
+
+write.csv(validation_data,
+          file = validation_path,
+          quote = FALSE, row.names = FALSE)
+
+training_path <- file.path(command_line_arguments$target,
+                           training_dataset_name)
+
+write.csv(training_data,
+          file = training_path,
+          quote = FALSE,
+          row.names = FALSE)
