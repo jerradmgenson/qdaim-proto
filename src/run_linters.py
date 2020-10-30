e0bd13f07e4e4013d0e919467e9023915cf98103
Jerrad <contact@jerradgenson.com>
2020-10-30 18:40:30 +0100

    Fix regular expressions for lintr

--- src/run_linters.py
+++ src/run_linters.py
@@ -132,8 +132,8 @@ def run_lintr(path):
                             errors=True,
                             report='')
 
-    errors = re.search(r'^Error:', lintr_stdout) is not None
-    issues = len(re.findall(r'^Warning:', lintr_stdout))
+    errors = re.search(r'Error:', lintr_stdout) is not None
+    issues = len(re.findall(r'Warning:', lintr_stdout))
     cloc_stdout = subprocess.check_output(['cloc', str(path), '--json']).decode('utf-8')
     cloc_json = json.loads(cloc_stdout)
     lines_of_code = cloc_json['SUM']['code']
