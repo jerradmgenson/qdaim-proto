59a605045a204075980a9cdb474e8b093158a092
Jerrad M Genson <contact@jerradgenson.com>
2020-12-06 16:59:56 +0100

    Add --test-set option to preprocess.R command line parser

--- src/preprocess.R
+++ src/preprocess.R
@@ -15,7 +15,7 @@
 # - Optionally convert target to a binary or ternary class.
 # - Rescale binary and ternary classes to range from -1 to 1.
 # - Randomize the row order.
-# - Split data into testing, training, and validation sets.
+# - Split data into test, train, and validation sets.
 #
 # Copyright 2020 Jerrad M. Genson
 #
@@ -31,8 +31,8 @@ library(mice)
 git_root <- system2('git', args=c('rev-parse', '--show-toplevel'), stdout=TRUE)
 source(file.path(git_root, "src/util.R"))
 
-# Name of the testing dataset
-testing_dataset_name <- "testing.csv"
+# Name of the test dataset
+test_dataset_name <- "testing.csv"
 
 # Name of the training dataset
 training_dataset_name <- "training.csv"
@@ -64,7 +64,7 @@ parse_command_line <- function(argv) {
                            default = "binary",
                            help = "Classification type. Possible values: 'binary', 'ternary', 'multiclass'")
 
-    parser <- add_argument(parser, "--testing-fraction",
+    parser <- add_argument(parser, "--test-fraction",
                            default = 0.2,
                            help = "Fraction of data to use for testing as a real number between 0 and 1.")
 
@@ -75,6 +75,10 @@ parse_command_line <- function(argv) {
     parser <- add_argument(parser, "--columns",
                            nargs = Inf,
                            help = "Columns to select from the input datasets.")
+
+    parser <- add_argument(parser, "--test-set",
+                           default = "",
+                           help = "Name of the dataset to sample test data from. Defaults to all datasets.")
 
     parse_args(parser, argv = argv)
 }
@@ -88,18 +92,19 @@ columns  <- if (!is.na(command_line_arguments$columns)) command_line_arguments$c
 
 # Read all CSV files from the given directory into a single dataframe.
 uci_dataset <- read_dir(command_line_arguments$source,
-                        columns = columns)
+                        columns = columns,
+                        test_set = command_line_arguments$test_set)
 
 # Remove rows where trestbps is 0.
-uci_dataset <- uci_dataset[uci_dataset$trestbps != 0, ]
+uci_dataset$df <- uci_dataset$df[uci_dataset$df$trestbps != 0, ]
 
 # Remove rows containing more than one NA.
-uci_dataset <- uci_dataset[rowSums(is.na(uci_dataset)) < 2, ]
+uci_wo_multi_na_rows <- uci_dataset$df[rowSums(is.na(uci_dataset$df)) < 2, ]
 
 # Impute missing data using single imputation.
-uci_dataset$restecg <- as.factor(uci_dataset$restecg)
-uci_dataset$fbs <- as.factor(uci_dataset$fbs)
-uci_mids <- mice(uci_dataset,
+uci_wo_multi_na_rows$restecg <- as.factor(uci_wo_multi_na_rows$restecg)
+uci_wo_multi_na_rows$fbs <- as.factor(uci_wo_multi_na_rows$fbs)
+uci_mids <- mice(uci_wo_multi_na_rows,
                  seed = command_line_arguments$random_seed,
                  method = c("", "", "", "", "logreg", "polyreg", "", "", "pmm", ""),
                  visit = "monotone",
@@ -144,21 +149,33 @@ if (command_line_arguments$classification_type == "binary") {
 # Shuffle order of rows in dataset.
 imputed_dataset <- imputed_dataset[sample(nrow(imputed_dataset)), ]
 
-# Split dataset into testing, training, and validation sets.
-testing_rows <- ceiling(nrow(imputed_dataset)
-                        * command_line_arguments$testing_fraction)
+# Split dataset into test, training, and validation sets.
+test_rows <- ceiling(nrow(imputed_dataset)
+                        * command_line_arguments$test_fraction)
+
+# Check that the number of test rows doesn't exceed the number of rows in the
+# specified test dataset if one was given.
+if (command_line_arguments$test_set != "") {
+    original_test_rows <-
+        length(uci_dataset$df[(rowSums(is.na(uci_dataset$df)) < 2)[1:uci_dataset$test_rows], ])
+
+    if (test_rows > original_test_rows) {
+        stop(sprintf("Too few samples in %s to create test set.",
+                     command_line_arguments$test_set))
+    }
+}
 
 validation_rows <-
     ceiling(nrow(imputed_dataset)
-            * command_line_arguments$validation_fraction) + testing_rows
+            * command_line_arguments$validation_fraction) + test_rows
 
-testing_data <- imputed_dataset[1:testing_rows, ]
-validation_data <- imputed_dataset[(testing_rows + 1):validation_rows, ]
+test_data <- imputed_dataset[1:test_rows, ]
+validation_data <- imputed_dataset[(test_rows + 1):validation_rows, ]
 training_data <- imputed_dataset[(validation_rows + 1):nrow(imputed_dataset), ]
 
 # Write datasets to the filesystem.
-testing_path <- file.path(command_line_arguments$target, testing_dataset_name)
-write.csv(testing_data, file = testing_path, quote = FALSE, row.names = FALSE)
+test_path <- file.path(command_line_arguments$target, test_dataset_name)
+write.csv(test_data, file = test_path, quote = FALSE, row.names = FALSE)
 validation_path <- file.path(command_line_arguments$target,
                              validation_dataset_name)
 
--- src/util.R
+++ src/util.R
@@ -11,18 +11,25 @@
 
 options(error = traceback)
 
-read_dir <- function(path, columns = NULL) {
+read_dir <- function(path, columns = NULL, test_set = "") {
     # Read all CSV files in a directory into a common dataframe.
     # Args:
     #   path: Path to the directory to read from.
     #   columns: The columns to select from the input datasets.
     #            Defaults to all available columns.
+    #   test_set: The dataset to use as the testing set. If given,
+    #             `read_dir` will place this data at the beginning of
+    #             the dataframe. It should be the name of the file
+    #             without the '.csv' extension.
 
     # Returns:
-    #   A dataframe with the combined data from all CSVs in `path`.
+    #   A named list with components `df`, a dataframe with the combined
+    #   data from all CSVs in `path`, and `test_rows`, which is the
+    #   number of rows in the input test dataset.
 
     csv_files <- dir(path, pattern=".csv")
     df <- NULL
+    test_rows <- 0
     for (csv_file in csv_files) {
         full_path <- file.path(path, csv_file)
         data_subset <- utils::read.csv(full_path)
@@ -31,9 +38,12 @@ read_dir <- function(path, columns = NULL) {
         }
         if (is.null(df)) {
             df <- data_subset
+        } else if (test_set != "" & (test_set == unlist(strsplit(csv_file, split = ".csv"))[1])) {
+            df <- rbind(data_subset, df)
+            test_rows <- length(data_subset)
         } else {
             df <- rbind(df, data_subset)
         }
     }
-    df
+    list(df = df, test_rows = test_rows)
 }
