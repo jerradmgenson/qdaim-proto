521410d98d309806d2880c5d4ad593255df83416
Jerrad M Genson <contact@jerradgenson.com>
2021-03-14 19:44:19 +0100

    Add system tests for the generated model

--- src/tests/system/test_preprocessed_data.py
+++ src/tests/system/test_preprocessed_data.py
@@ -2,6 +2,12 @@
 System tests for training, validation, and testing datasets created by
 preprocess.R.
 
+Copyright 2021 Jerrad M. Genson
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 """
 
 import unittest
